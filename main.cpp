fa779bfebf2a92b1472e49bc2d8d51d8dfa2f75d
Siyuan Ren <netheril96@gmail.com>
2022-11-29 12:07:58 +0800

    Refactor

--- main.cpp
+++ main.cpp
@@ -2,31 +2,43 @@
 
 #include <cryptopp/gcm.h>
 #include <cryptopp/aes.h>
+#include <openssl/evp.h>
 
 namespace
 {
     constexpr unsigned char KEY[16] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
     constexpr unsigned char IV[12] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 
-    unsigned encrypt_sum_with_cryptopp(const std::vector<unsigned char> &input)
+    class CryptoppBench
     {
-        std::vector<unsigned char> output(input.size() + 16);
+    private:
         CryptoPP::GCM<CryptoPP::AES>::Encryption enc;
-        enc.SetKeyWithIV(KEY, sizeof(KEY), IV);
-        enc.EncryptAndAuthenticate(output.data(), output.data() + input.size(), 16,
-                                   IV, sizeof(IV), nullptr, 0, input.data(), input.size());
-        unsigned result = 0;
-        for (auto c : output)
+        std::vector<unsigned char> output;
+
+    public:
+        CryptoppBench()
         {
-            result += c;
+            enc.SetKeyWithIV(KEY, sizeof(KEY), IV);
         }
-        return result;
-    }
+
+        unsigned encrypt_sum(const std::vector<unsigned char> &input)
+        {
+            output.resize(input.size() + 16);
+            enc.EncryptAndAuthenticate(output.data(), output.data() + input.size(), 16,
+                                       IV, sizeof(IV), nullptr, 0, input.data(), input.size());
+            unsigned result = 0;
+            for (auto c : output)
+            {
+                result += c;
+            }
+            return result;
+        }
+    };
 }
 
 int main()
 {
     std::vector<unsigned char> input(1 << 30, 233);
-    printf("Sum %u\n", encrypt_sum_with_cryptopp(input));
+    printf("Sum %u\n", CryptoppBench().encrypt_sum(input));
     return 0;
 }
